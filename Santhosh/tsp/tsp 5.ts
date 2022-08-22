a8a9459356a4f857dd2c399e0955f2406127a445
Santhoshkumarale <santhoshkumarale401@gmail.com>
2022-08-22 15:10:07 +0530

    typescript day 9

---
+++ Santhosh/tsp/tsp 5.ts
@@ -0,0 +1,25 @@
+export { }
+
+class Sample {
+    private a = 100;
+    protected b = 200;
+
+    public showData() {
+        console.log(this.a);
+        console.log(this.b);
+    }
+}
+class Sample1 extends Sample {
+    c: number;
+    public cal() {
+        this.c = 100 + this.b;
+    }
+    public show(): void {
+        this.showData();
+        console.log(this.c);
+    }
+}
+
+let sampleObj = new Sample1();
+sampleObj.cal();
+sampleObj.show();
