cbfac7e5fc6b3b34aaae0328903d9b06db72df5d
mounikaboinapally <mounika.boinapally12@gmail.com>
2022-08-23 15:01:05 +0530

    Typescript and Angular

---
+++ MounikaB/Typescript/ex6.ts
@@ -0,0 +1,26 @@
+export { }
+class Sample {
+    private a = 100;
+    protected b = 200;
+    public showData() {
+        console.log(this.a);
+        console.log(this.b);
+    }
+}
+
+class Sample1 extends Sample {
+    c: number;
+    public cal() {
+        this.c = 100 + this.b;
+    }
+    public show(): void {
+        this.showData();
+        console.log(this.c);
+    }
+
+}
+
+
+let SampleObj = new Sample1();
+SampleObj.cal();
+SampleObj.show();
