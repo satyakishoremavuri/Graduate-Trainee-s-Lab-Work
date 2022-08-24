7ef0841ae25ceef22a611481a04368422934ce98
shreyat17 <shreyatr178@gmail.com>
2022-08-24 15:10:52 +0530

    Typescript

---
+++ Shreya/Typescript/ex1.ts
@@ -0,0 +1,30 @@
+/*
+Data Types:-
+1.Primitive types
+number,string,boolean,null,undefined,symbol,any
+2.Reference types
+array,object,class,interface
+
+
+Variable declaration:-
+<variable_name>: <data_type>=<initial_value>;*/
+let firstName:string="Shreya";
+let lastName="Tr";  //inferred type
+let a:number=100;
+let result:boolean=true;
+
+let anyValue;
+anyValue=100;
+anyValue="Shreya";
+anyValue="True";
+anyValue={};
+anyValue.result=true;
+anyValue();
+anyValue=[];
+
+
+
+
+
+
+
