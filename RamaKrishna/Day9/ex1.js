189030019fb87c63d721e53b27c514134ffa45c0
srksbce <srk.sbce@gmail.com>
2022-08-27 03:59:22 +0530

    coomit

--- RamaKrishna/Day9/ex1.js
+++ RamaKrishna/Day9/ex1.js
@@ -1,4 +1,21 @@
-function sum(a, b) {
-    return a + b;
-}
-console.log(sum(10, 20));
+"use strict";
+exports.__esModule = true;
+var Sample = /** @class */ (function () {
+    function Sample() {
+    }
+    //public
+    Sample.prototype.setData = function () {
+        // let firstName: string;
+        // firstName = "Rama";
+        this.lastName = "Krishna";
+        this.firstName = "Rama";
+    };
+    Sample.prototype.showData = function () {
+        console.log(this.firstName);
+        console.log(this.lastName);
+    };
+    return Sample;
+}());
+var sampleobj = new Sample();
+sampleobj.setData();
+sampleobj.showData();
--- RamaKrishna/Day9/ex1.ts
+++ RamaKrishna/Day9/ex1.ts
@@ -1,6 +1,28 @@
-
-function sum(a: number, b: number) {
-    return a + b;
+export {
 
 }
-console.log(sum(10, 20));
+class Sample {
+    //private
+    private firstName: string;
+    private lastName: string;
+    //public
+    public setData(): void {
+        // let firstName: string;
+        // firstName = "Rama";
+        this.lastName = "Krishna";
+        this.firstName = "Rama";
+    }
+    public showData() {
+        console.log(this.firstName);
+        console.log(this.lastName);
+
+    }
+
+}
+let sampleobj = new Sample();
+sampleobj.setData();
+sampleobj.showData();
+
+
+
+
