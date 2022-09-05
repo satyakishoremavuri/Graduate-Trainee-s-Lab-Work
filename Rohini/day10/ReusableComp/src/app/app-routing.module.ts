8d6afea71a50104befb0f3d26105df394b69ce40
RohiniReddy41 <chinnirohinireddy@gmail.com>
2022-09-05 14:54:43 +0530

    practised

--- Rohini/day10/ReusableComp/src/app/app-routing.module.ts
+++ Rohini/day10/ReusableComp/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer/customer.component';
+import { EmployeeComponent } from './employee/employee/employee.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'employee',loadChildren:()=>import("./employee/employee.module").then(x=>x.EmployeeModule)},
-  {path:'customer',loadChildren:()=>import("./customer/customer.module").then(x=>x.CustomerModule)},
-  {path:'student',loadChildren:()=>import("./student/student.module").then(x=>x.StudentModule)},
-  {path:'nested',loadChildren:()=>import("./nested/nested.module").then(x=>x.NestedModule)}
+  {path:'customer',loadChildren:()=>import("./customer/customer.module").then(x=>x.CustomerModule)}
 ];
 
 @NgModule({
