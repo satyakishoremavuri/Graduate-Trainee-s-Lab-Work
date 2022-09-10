6fb958a569038aa3e080fe116465bb2e77d81fc8
Arjunc331 <arjunc331@gmail.com>
2022-09-10 13:58:56 +0530

    practice

---
+++ Arjun/ConApp1/ConApp1/Class15.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    //Single Inheriatnce
+
+    class Sample        //Base Class
+    {
+        protected int a;
+        protected int b;
+
+        public void GetData()
+        {
+            Console.Write("Enter a Value :");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter b Value :");
+            b = int.Parse(Console.ReadLine());
+        }
+        public void Show()
+        {
+            Console.WriteLine("a Value is :" + a);
+            Console.WriteLine("b Value is :" + b);
+        }
+    }
+    class Sample1 : Sample  //Derived Class
+    {
+        int c;
+        public void cal()
+        {
+            c = a + b;
+        }
+        public new void Show()
+        {
+            base.Show();
+            Console.WriteLine("c Value is :" + c);
+        }
+    }
+    internal class Class15
+    {
+        static void Main(string[] args)
+        {
+            Sample1 sample = new Sample1();
+            sample.GetData();
+            sample.cal();
+            sample.Show();
+        }
+    }
+}
---
+++ Arjun/ConApp1/ConApp1/Class5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    //Class Variables or Instance Variables and Static Variables
+    internal class Class5
+    {
+        int EmpNumber = 101;           //instance variable
+        static int StudNumber = 2002;   //static variable
+
+        public void Show()              //Instance Method
+        {
+            Console.WriteLine("Employee Number  :" + EmpNumber);
+            Console.WriteLine("Student Number   :" + StudNumber);
+            Display();
+            GetData();
+        }
+        public void Display()           //Instance Method
+        {
+
+        }
+        public static void GetData()    //Static Method
+        {
+
+        }
+        static void Main(string[] args)    //Static Method
+        {
+            Console.WriteLine("Student Number  :" + StudNumber);
+            GetData();
+
+            Class5 class5 = new Class5();
+            Console.WriteLine("Employee Number  :" + class5.EmpNumber);
+            class5.Show();
+        }
+    }
+}
