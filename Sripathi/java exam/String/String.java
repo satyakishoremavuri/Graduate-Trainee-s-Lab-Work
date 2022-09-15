960c70ba2e4877d9c72c597a92f55d345ee3056d
sripathisri <sripathisri8@gmail.com>
2022-09-15 18:50:34 +0530

    update

---
+++ Sripathi/java exam/String/String.java
@@ -0,0 +1,17 @@
+
+class String{
+    public static void main(String[] args) {
+      java.lang.String str = "my name is sripathi";
+
+      java.lang.String[] a = str.split(" ");
+
+      for(int i = a.length-1;i>=0;i++){
+        System.out.print(a[i]+" ");
+      }
+
+
+
+    }
+}
+
+
