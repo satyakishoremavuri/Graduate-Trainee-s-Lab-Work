06fd1931c47b29cdc3b13a134d0f641c48ebdb77
sripathisri <sripathisri8@gmail.com>
2022-09-15 17:46:04 +0530

    update

---
+++ Sripathi/java exam/Table/Table.java
@@ -0,0 +1,15 @@
+class Table{
+
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        for(int i = 0; i <= 10;i++){
+            System.out.println(num + "x" + i + "=" + i*num);
+        }
+
+        
+    }
+
+
+}
