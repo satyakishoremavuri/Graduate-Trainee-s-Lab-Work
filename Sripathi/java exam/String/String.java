64d470b031d9cbf0979db4d54c7700127b3383cb
sripathisri <sripathisri8@gmail.com>
2022-09-15 22:11:18 +0530

    update

--- Sripathi/java exam/String/String.java
+++ Sripathi/java exam/String/String.java
@@ -1,13 +1,21 @@
 
+
 class String{
     public static void main(String[] args) {
-      java.lang.String str = "my name is sripathi";
+      java.lang.String s = "my name sripathi";
 
-      java.lang.String[] a = str.split(" ");
+	      int len = s.length();
+	      java.lang.String rev = "";
 
-      for(int i = a.length-1;i>=0;i++){
-        System.out.print(a[i]+" ");
-      }
+	      
+
+	      for(int i = len-1;i>=0;i--){
+	        rev = rev + s.charAt(i);
+	       
+	      }
+
+	      System.out.println(rev);
+
 
 
 
