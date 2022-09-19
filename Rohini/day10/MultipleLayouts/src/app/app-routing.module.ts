28ceaa197fa5728e28a295ed5d8a5abe36d68020
RohiniReddy41 <chinnirohinireddy@gmail.com>
2022-09-19 11:31:49 +0530

    practise

--- Rohini/day10/MultipleLayouts/src/app/app-routing.module.ts
+++ Rohini/day10/MultipleLayouts/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { AdminLayoutComponent } from './Layout/admin-layout/admin-layout.component';
 import { AuthenticationLayoutComponent } from './Layout/authentication-layout/authentication-layout.component';
 import { ADMIN_ROUTES } from './Routing/admin-routing';
@@ -7,7 +8,7 @@ import { AUTHENTICATION_ROUTES } from './Routing/authentication-routing';
 
 const routes: Routes = [
   {path:'',component:AuthenticationLayoutComponent,children:AUTHENTICATION_ROUTES},
-  {path:'admin',component:AdminLayoutComponent,children:ADMIN_ROUTES}
+  {path:'admin',component:AdminLayoutComponent,children:ADMIN_ROUTES,canActivate: [AuthGuard],canDeactivate:[AuthGuard]}
 ];
 
 @NgModule({
